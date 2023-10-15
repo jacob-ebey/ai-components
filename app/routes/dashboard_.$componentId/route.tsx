2044a866400d7f3fbbb30c4475f86d92e192c115
Jacob Ebey <jacob.ebey@live.com>
2023-10-14 19:03:11 -0700

    remove unused import

--- app/routes/dashboard_.$componentId/route.tsx
+++ app/routes/dashboard_.$componentId/route.tsx
@@ -21,7 +21,6 @@ import { ComponentPreview } from "~/components/component-preview";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
-import { Textarea } from "~/components/ui/textarea";
 import {
   db,
   componentRevisionTable,
